1c2aba0bf0c11a3877557e2f7a987f50b86c87be
Aviral Saxena <64943664+AviralSaxena@users.noreply.github.com>
2023-08-14 05:26:26 -0500

    Add "Color Mode" and Reduce Redundancy

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
-const gridContainer=document.querySelector('.gridContainer');
+const gridContainer = document.querySelector('.gridContainer');
+let currentSideLength = 16;
 
 function createGrid(sideLength) {
-
     gridContainer.innerHTML = '';
 
-    gridContainer.style.gridTemplateColumns=`repeat(${sideLength}, 1fr)`;
-    gridContainer.style.gridTemplateRows=`repeat(${sideLength}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${sideLength}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${sideLength}, 1fr)`;
 
-    for (let i=0; i<sideLength*sideLength; i++)
-        {
-            const gridItem = document.createElement('div');
-            gridItem.classList.add('grid-item');
-            gridContainer.appendChild(gridItem);
+    for (let i = 0; i < sideLength * sideLength; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridContainer.appendChild(gridItem);
 
-            gridItem.addEventListener('mouseover', () => {
+        gridItem.addEventListener('mouseover', () => {
+            if (colorModeActive) {
                 gridItem.classList.add('newColor');
-            });
-
-            const eraserButton = document.getElementById('eraser-button');
-            eraserButton.addEventListener('click',()=>{
-                gridItem.addEventListener('mouseover', () => {
-                    gridItem.classList.remove('newColor');
-                });
-            });
-        }
+            } else if (eraserModeActive) {
+                gridItem.classList.remove('newColor');
+            }
+        });
+    }
 }
 
-createGrid(16);
+createGrid(currentSideLength);
 
+const gridSizeSlider = document.getElementById('gridSizeSlider');
 const gridSizeLabel = document.getElementById('gridSizeLabel');
+const colorModeButton = document.getElementById('color-mode-button');
+const eraserModeButton = document.getElementById('eraser-mode-button');
+const resetButton = document.getElementById('reset-button');
 
-gridSizeSlider.addEventListener('change', () => {
-    const newSideLength = parseInt(gridSizeSlider.value);
-    gridSizeLabel.textContent = `Grid Size: ${newSideLength}x${newSideLength}`;
-    createGrid(newSideLength);
+let colorModeActive = true;
+let eraserModeActive = false;
+
+gridSizeSlider.addEventListener('input', () => {
+    currentSideLength = parseInt(gridSizeSlider.value);
+    gridSizeLabel.textContent = `Grid Size: ${currentSideLength}x${currentSideLength}`;
+    createGrid(currentSideLength);
 });
 
-const resetContainer = document.getElementById('reset-button');
+colorModeButton.addEventListener('click', () => {
+    colorModeActive = true;
+    eraserModeActive = false;
+});
 
-resetContainer.addEventListener('click',()=>{
-    const currentSideLength = parseInt(gridSizeSlider.value);
+eraserModeButton.addEventListener('click', () => {
+    colorModeActive = false;
+    eraserModeActive = true;
+});
+
+resetButton.addEventListener('click', () => {
     createGrid(currentSideLength);
-})
+});
